8f562c9139fe8f184bd0b52f5f08ca7e15419f36
rhodinemma <rhodinemma10@gmail.com>
2023-04-07 08:38:03 +0300

    feat: add restaurant page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,30 @@
 export default function Home() {
   return (
-    <main></main>
+    <main className="bg-gray-100 min-h-screen w-90">
+      <main className="max-w-screen-2xl m-auto bg-white">
+        {/* NAVBAR */}
+        <nav className="bg-white p-2 flex justify-between">
+          <a href="" className="font-bold text-gray-700 text-2xl font-comf">
+            Reservation Next
+          </a>
+          <div>
+            <div className="flex">
+              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
+                Signin
+              </button>
+              <button className="border p-1 px-4 rounded">Signup</button>
+            </div>
+          </div>
+        </nav>
+        {/* NAVBAR */}
+        {/* HEADER */}
+        <div className="h-96 overflow-hidden">
+          <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
+            <h1 className="text-7xl text-white capitalize text-shadow text-center">Milestones Grill (Toronto)</h1>
+          </div>
+        </div>
+        {/* HEADER */}
+      </main>
+    </main>
   );
 }
