cd07a6307b8617c239f16e1b5a67b564ca8bb778
rhodinemma <rhodinemma10@gmail.com>
2023-04-07 09:22:32 +0300

    feat: add image gallery

--- app/page.tsx
+++ app/page.tsx
@@ -54,6 +54,49 @@ export default function Home() {
               <p>The classics you love prepared with a perfect twist, all served up in an atmosphere that feels just right. That's the Milestones promise. So whether you're celebrating a milestone, making the most of Happy Hour or enjoying brunch with friends, you can be sure that every Milestones experience is a simple and perfectly memorable one.</p>
             </div>
             {/* DESCRIPTION */}
+            {/* IMAGES */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">
+                7 photos
+              </h1>
+              <div className="flex flex-wrap">
+                <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+                <img className="w-56 h-44 mr-1 mb-1" src="" alt="" />
+              </div>
+            </div>
+            {/* IMAGES */}
+            {/* REVIEWS */}
+            <div>
+              <h1 className="font-bold text-3xl mt-10 mb-7 border-b pb-5">What 100 people are saying</h1>
+              <div>
+                {/* REVIEW CARD */}
+                <div className="border-b pb-7 mb-7">
+                  <div className="flex">
+                    <div className="w-1/6 flex flex-col items-center">
+                      <div className="rounded-full bg-blue-400 w-16 h-16 flex items-center justify-center">
+                        <h2 className="text-white text-2xl">NR</h2>
+                      </div>
+                      <p className="text-center font-bold">Nagwere Rhodin</p>
+                    </div>
+                    <div className="ml-10 w-5/6">
+                      <div className="flex items-center">
+                        <div className="flex mr-5">
+                          *****
+                        </div>
+                      </div>
+                      <div className="mt-5">
+                        <p className="text-lg font-light">The Greek restaurant's service was top-notch and made our experience even better. I was impressed by the quality of the ingredients used in the Greek restaurant's dishes.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {/* REVIEW CARD */}
+              </div>
+            </div>
+            {/* REVIEWS */}
           </div>
         </div>
         {/* DESCRIPTION PORTION */}
