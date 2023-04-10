44069674efdf56e8570926d61e8c95ea1a43faea
rhodinemma <rhodinemma10@gmail.com>
2023-04-10 18:21:07 +0300

    chore: creating reusable components

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,23 +1,12 @@
 import Link from "next/link";
+import NavBar from "../../../components/NavBar";
 
 export default function RestaurantMenu() {
     return (
         <main className="bg-gray-100 min-h-screen w-90">
             <main className="max-w-screen-2xl m-auto bg-white">
                 {/* NAVBAR */}
-                <nav className="bg-white p-2 flex justify-between">
-                    <Link href="/" className="font-bold text-gray-700 text-2xl font-comf">
-                        Reservation Next
-                    </Link>
-                    <div>
-                        <div className="flex">
-                            <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                                Signin
-                            </button>
-                            <button className="border p-1 px-4 rounded">Signup</button>
-                        </div>
-                    </div>
-                </nav>
+                <NavBar />
                 {/* NAVBAR */}
                 {/* HEADER */}
                 <div className="h-96 overflow-hidden">
