1c3969794723f5b92973541d2eefdee8bc5e7431
rhodinemma <rhodinemma10@gmail.com>
2023-04-11 15:02:46 +0300

    feat: using nested layouts in components

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -10,23 +10,20 @@ import Images from "./components/images";
 export default function RestaurantDetails() {
     return (
         <>
-            <Header />
             {/* DESCRIPTION PORTION */}
-            <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-                <div className="bg-white w-[70%] rounded p-3 shadow">
-                    <RestaurantNavBar />
-                    <Title />
-                    <Rating />
-                    <Description />
-                    <Images />
-                    <Reviews />
-                </div>
-                {/* RESERVATION CARD PORTION */}
-                <div className="w-[28%] relative text-reg">
-                    <ReservationCard />
-                </div>
-                {/* RESERVATION CARD PORTION */}
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+                <RestaurantNavBar />
+                <Title />
+                <Rating />
+                <Description />
+                <Images />
+                <Reviews />
             </div>
+            {/* RESERVATION CARD PORTION */}
+            <div className="w-[28%] relative text-reg">
+                <ReservationCard />
+            </div>
+            {/* RESERVATION CARD PORTION */}
             {/* DESCRIPTION PORTION */}
         </>
     );
