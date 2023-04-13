ededd5903eeb1950327c819b66bc4489d4c388c3
rhodinemma <rhodinemma10@gmail.com>
2023-04-13 07:50:09 +0300

    feat: fetching the restaurant's menu

--- app/restaurant/[slug]/components/MenuCard.tsx
+++ app/restaurant/[slug]/components/MenuCard.tsx
@@ -1,10 +1,11 @@
-export default function MenuCard() {
+import { Item } from "@prisma/client";
+
+export default function MenuCard({ item }: { item: Item }) {
     return (
         <div className="border rounded p-3 w-[49%] mb-3">
-            <h3 className="font-bold text-lg">Surf and Turf</h3>
-            <p className="font-light mt-1 text-sm">Grilled 8oz New York Striploin and 6oz Lobster
-                Tail</p>
-            <p className="mt-7">$79.00</p>
+            <h3 className="font-bold text-lg">{item.name}</h3>
+            <p className="font-light mt-1 text-sm">{item.description}</p>
+            <p className="mt-7">{item.price}</p>
         </div>
     );
 }
